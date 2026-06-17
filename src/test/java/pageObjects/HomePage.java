32f29159eac17e0785979ed4b38ff3c5e1902fc5
HarshG04 <goyalharsh502@gmail.com>
2026-06-17 14:58:23 +0530

    Tested TS005_HomePageInfo

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -3,6 +3,7 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage{
     //constructor
@@ -15,10 +16,9 @@ public class HomePage extends BasePage{
     @FindBy(xpath = "//div[@id='navbarNav']//a[contains(text(),'Login')]") WebElement lnkLogin;
     @FindBy(xpath = "//a[contains(text(),'Blood Availability')]") WebElement lnkBloodAvailability;
     @FindBy(xpath = "//button[contains(text(),'I Need Blood')]") WebElement btnINeedBlood;
-
-
-
-
+    @FindBy(xpath = "//h2[contains(text(),'Why Donate Blood?')]/ancestor::div[@class='container']") WebElement sectionWhyDonateBlood;
+    @FindBy(xpath = "//h2[contains(text(),'Benefits of Donating Blood')]/ancestor::section") WebElement sectionBenefitsOfDonatingBlood;
+    @FindBy(xpath = "//h2[contains(text(),'Eligibility Criteria')]/ancestor::section") WebElement sectionEligibilityCriteria;
 
 
 
@@ -34,5 +34,21 @@ public class HomePage extends BasePage{
     }
     public void clickINeedBlood(){
         btnINeedBlood.click();
+    }
+    public boolean checkWhyDonateBloodVisibility(){
+        wait.until(ExpectedConditions.visibilityOf(sectionWhyDonateBlood));
+        js.executeScript("arguments[0].scrollIntoView()",sectionWhyDonateBlood);
+        return sectionWhyDonateBlood.isDisplayed();
+    }
+
+    public boolean checkBenefitsOfDonatingBloodVisibility(){
+        wait.until(ExpectedConditions.visibilityOf(sectionBenefitsOfDonatingBlood));
+        js.executeScript("arguments[0].scrollIntoView()",sectionBenefitsOfDonatingBlood);
+        return sectionBenefitsOfDonatingBlood.isDisplayed();
+    }
+    public boolean checkEligibilityCriteriaVisibility(){
+        wait.until(ExpectedConditions.visibilityOf(sectionEligibilityCriteria));
+        js.executeScript("arguments[0].scrollIntoView()",sectionEligibilityCriteria);
+        return sectionEligibilityCriteria.isDisplayed();
     }
 }
