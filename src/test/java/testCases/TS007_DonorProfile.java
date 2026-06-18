6e567693d081ba9e62012f8de920725b6b48cbf7
Hemanth Yerra <hemanthyerra9@gmail.com>
2026-06-18 17:05:47 +0530

    Tested TC022_VerifyDonorCannotUpdateEmailToExistingEmail

--- src/test/java/testCases/TS007_DonorProfile.java
+++ src/test/java/testCases/TS007_DonorProfile.java
@@ -280,6 +280,112 @@ public class TS007_DonorProfile extends BaseClass {
     }
 
     @Test(priority = 4,dataProvider = "donorLoginData",dataProviderClass = LoginDataProvider.class)
+    public void TC022_VerifyDonorCannotUpdateEmailToExistingEmail(String email,String pwd){
+        logger.info("=========================================================");
+        logger.info("STARTING TEST CASE: TC022_VerifyDonorCannotUpdateEmailToExistingEmail");
+        logger.info("=========================================================");
+
+        try{
+            HomePage homePage = new HomePage(driver);
+            logger.info("Navigating to Registration page from Home page...");
+            homePage.clickRegister();
+
+            RegisterPage registerPage = new RegisterPage(driver);
+            logger.info("PHASE 1: Generating unique user dataset to seed the database...");
+            String[] userData = RandomDataGeneratorUtil.randomUserDataGenerator();
+            logger.info("Target email to seed: '" + userData[1] + "'");
+
+            logger.info("Populating form fields for the initial baseline user...");
+            RandomDataGeneratorUtil.submitUserData(registerPage, userData);
+
+            logger.info("Clicking on the Agree Terms checkbox...");
+            registerPage.clickAgreeTerms();
+
+            logger.info("Submitting the initial registration...");
+            registerPage.clickRegister();
+
+            String alertMsg = registerPage.getAlertMessage();
+            logger.info("Initial registration system response: '" + alertMsg + "'");
+
+            logger.info("Verifying baseline user registered successfully...");
+            Assert.assertEquals(alertMsg, "User Registered Successfully", "Baseline registration failed!");
+
+            logger.info("PHASE 2: Navigating back to the login screen to test duplicate constraint...");
+            logger.info("Entering login credentials");
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.waitForUrlToContain("/login");
+            loginPage.setEmail(email);
+            loginPage.setPassword(pwd);
+
+            logger.info("Clicking Login button");
+            loginPage.clickLogin();
+
+            logger.info("Verifying login success (Dashboard page)");
+            Assert.assertTrue(homePage.waitForUrlToContain("/donor/dashboard"),
+                    "Login failed: Dashboard URL not loaded");
+
+            logger.info("Navigating to Edit Profile page");
+            DonorDashboardPage donorDash = new DonorDashboardPage(driver);
+            donorDash.clickEditProfile();
+
+            logger.info("Verifying Edit Profile page load");
+            Assert.assertTrue(donorDash.waitForUrlToContain("/edit-profile"),
+                    "Edit Profile page not loaded");
+
+            DonorProfilePage donorProfile = new DonorProfilePage(driver);
+
+            logger.info("Checking if Email field is editable");
+
+            Assert.assertTrue(donorProfile.isEmailEditable(),
+                    "Email field is NOT editable");
+
+            logger.info("Updating Email to: " + userData[1]);
+            donorProfile.enterEmail(userData[1]);
+
+            logger.info("Clicking Save Changes");
+            donorProfile.clickSaveChanges();
+
+            logger.info("Validating duplicate email behavior...");
+
+            // Case 1: If it navigates to dashboard → BUG → FAIL TEST
+            boolean isRedirected = donorProfile.waitForUrlToContain("/donor/dashboard");
+
+            if (isRedirected) {
+                logger.error("BUG: Duplicate phone number was accepted and profile updated!");
+                Assert.fail("Test FAILED: System allowed duplicate phone number update");
+                donorDash.clickUserDropDown();
+                donorDash.clickLogout();
+            }
+            // Case 2: Check error message (expected behavior)
+            else{
+                logger.info("Duplicate phone number correctly rejected");
+                Assert.assertTrue(true);
+            }
+
+        }catch (AssertionError ae) {
+            logger.error("ASSERTION FAILED : " + ae.getMessage());
+            throw ae; // rethrow to mark test as FAILED
+
+        } catch (Exception e) {
+            logger.error("EXCEPTION OCCURRED : " + e.getMessage(), e);
+            Assert.fail("Test failed due to unexpected exception");
+        }
+        finally {
+            try {
+                DonorDashboardPage donorDash = new DonorDashboardPage(driver);
+                donorDash.clickUserDropDown();
+                donorDash.clickLogout();
+                logger.info("Logged out in finally block");
+            } catch (Exception e) {
+                logger.warn("Logout skipped: " + e.getMessage());
+            }
+            logger.info("=========================================================");
+            logger.info("ENDING TEST CASE: TC023_VerifyDonorCannotUpdatePhoneNumberToExistingUserNumber");
+            logger.info("=========================================================");
+        }
+    }
+
+    @Test(priority = 5,dataProvider = "donorLoginData",dataProviderClass = LoginDataProvider.class)
     public void TC023_VerifyDonorCannotUpdatePhoneNumberToExistingUserNumber(String email,String pwd){
         logger.info("=========================================================");
         logger.info("STARTING TEST CASE: TC023_VerifyDonorCannotUpdatePhoneNumberToExistingUserNumber");
